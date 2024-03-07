33667a2b6f3000306d4242a22256fbb15f8223e4
Simon Dunn <simondunn90@gmail.com>
2024-03-07 15:25:15 +0000

    secrets.baseline

--- app/index.js
+++ app/index.js
@@ -1,10 +1,12 @@
 require('./insights').setup()
 require('log-timestamp')
+const { startMessaging } = require('./messaging')
 const { createServer } = require('./server')
 
 const init = async () => {
   const server = await createServer()
   await server.start()
+  await startMessaging()
   console.log('Server running on %s', server.info.uri)
 }
 
