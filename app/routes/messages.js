44bd88b662bd0c5656d0c5956c9a29ca3a2c0b4b
Rana Salem <ranatasalem@gmail.com>
2024-04-15 13:57:26 +0100

    update messages route

--- app/routes/messages.js
+++ app/routes/messages.js
@@ -4,19 +4,6 @@ const { GET } = require('../constants/http-verbs')
 module.exports = [
   {
     method: GET,
-    path: '/messages',
-    handler: async (request, h) => {
-      try {
-        const result = await db.messages.findAll()
-        console.log('result: ', result)
-        return h.response({ data: result }).code(200)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-  },
-  {
-    method: GET,
     path: '/messages/{sbi}',
     handler: async (request, h) => {
       try {
@@ -26,6 +13,25 @@ module.exports = [
           }
         })
         console.log('Query: select by sbi')
+        return h.response({ data: result }).code(200)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  },
+  {
+    method: GET,
+    path: '/messages/{sbi}/{id}',
+    handler: async (request, h) => {
+      try {
+        console.log('SBI and ID: ', request.params.sbi, request.params.id)
+        const result = await db.messages.findOne({
+          where: {
+            sbi: request.params.sbi,
+            messageId: request.params.id
+          }
+        })
+        console.log('Query: select by sbi then by id')
         return h.response({ data: result }).code(200)
       } catch (error) {
         console.log(error)
