daea7274d851f8e2ed7a47a1b63fa2cc2c9a913b
Rana Salem <ranatasalem@gmail.com>
2024-04-18 14:48:23 +0100

    add initial functionality to input dates w/ uk format

--- app/messaging/save-to-database.js
+++ app/messaging/save-to-database.js
@@ -2,13 +2,21 @@ const db = require('../data')
 
 const saveToDatabase = async (message) => {
   try {
+    const requestedDate = new Date(message.body.requestedDate)
+    const formattedDate = `${requestedDate
+      .getDate()
+      .toString()
+      .padStart(2, '0')}/${(requestedDate.getMonth() + 1)
+      .toString()
+      .padStart(2, '0')}/${requestedDate.getFullYear()}`
+
     await db.messages.create({
       scheme: message.body.scheme,
       tags: message.body.tags,
       crn: message.body.crn,
       sbi: message.body.sbi,
       content: message.body.content,
-      requestedDate: message.body.requestedDate
+      requestedDate: formattedDate
     })
   } catch (error) {
     console.error('ERROR: ', error)
