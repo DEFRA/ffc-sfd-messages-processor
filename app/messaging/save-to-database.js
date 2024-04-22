9caecb40d9ea605c334b1a9836278d9e8c083ca5
Rana Salem <ranatasalem@gmail.com>
2024-04-22 15:52:47 +0100

    update formatted date function

--- app/messaging/save-to-database.js
+++ app/messaging/save-to-database.js
@@ -1,9 +1,15 @@
 const db = require('../data')
 
+const formatDateToUK = (date) => {
+  const day = String(date.getDate()).padStart(2, '0')
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const year = date.getFullYear()
+  return `${day}-${month}-${year}`
+}
+
 const saveToDatabase = async (message) => {
   try {
-    const requestedDate = new Date(message.body.requestedDate)
-    const formattedDate = requestedDate.toLocaleDateString('en-GB')
+    const formattedDate = formatDateToUK(new Date(message.body.requestedDate))
 
     await db.messages.create({
       scheme: message.body.scheme,
