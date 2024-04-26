15c1e934775142b01c7262e2c52c16c4edffc45d
Rana Salem <ranatasalem@gmail.com>
2024-04-26 15:49:05 +0100

    361164 Update requestedDate to use UK format (#8)

--- app/messaging/save-to-database.js
+++ app/messaging/save-to-database.js
@@ -2,16 +2,19 @@ const db = require('../data')
 
 const saveToDatabase = async (message) => {
   try {
+    const [day, month, year] = message.body.requestedDate.split('-')
+
     await db.messages.create({
       scheme: message.body.scheme,
       tags: message.body.tags,
       crn: message.body.crn,
       sbi: message.body.sbi,
       content: message.body.content,
-      requestedDate: message.body.requestedDate
+      requestedDate: new Date(`${year}-${month}-${day}`)
     })
   } catch (error) {
-    console.error('ERROR: ', error)
+    console.error(error)
   }
 }
+
 module.exports = { saveToDatabase }
