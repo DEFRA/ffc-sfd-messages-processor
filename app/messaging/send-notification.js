874cd0dbde30d0e8be1c6951242698428138d5bf
Rana Salem <ranatasalem@gmail.com>
2024-07-11 16:26:46 +0100

    Update Notify test email to use env var (#13)

--- app/messaging/send-notification.js
+++ app/messaging/send-notification.js
@@ -3,12 +3,14 @@ const NotifyClient = require('notifications-node-client').NotifyClient
 
 const sendNotification = async (message) => {
   const notifyClient = new NotifyClient(process.env.NOTIFY_API_KEY)
-  await notifyClient.sendEmail(process.env.NOTIFY_TEMPLATE_ID, 'rana.salem@defra.gov.uk', {
-    personalisation: {
-      heading: message.body.content.heading,
-      content: message.body.content.body
-    },
-    reference: uuidv4()
-  })
+  await notifyClient.sendEmail(
+    process.env.NOTIFY_TEMPLATE_ID,
+    process.env.NOTIFY_TEST_EMAIL, {
+      personalisation: {
+        heading: message.body.content.heading,
+        content: message.body.content.body
+      },
+      reference: uuidv4()
+    })
 }
 module.exports = { sendNotification }
