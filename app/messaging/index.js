9ce04368604be5bc3c51a444be7d960f740e347c
Rana Salem <ranatasalem@gmail.com>
2024-07-26 14:01:38 +0100

    381534 Spike: Email link login (#16)

--- app/messaging/index.js
+++ app/messaging/index.js
@@ -1,17 +1,15 @@
 const util = require('util')
 const { messageConfig } = require('../config')
 const { MessageReceiver } = require('ffc-messaging')
-const { saveToDatabase } = require('./save-to-database')
 const { sendNotification } = require('./send-notification')
 
 const handleMessage = async (message, receiver) => {
   try {
     console.log('Received message: ', message.body)
-    await saveToDatabase(message)
     await sendNotification(message)
     await receiver.completeMessage(message)
   } catch (err) {
-    console.error('Error with message-processor message:', util.inspect(err.message, false, null, true))
+    console.error('Error with processing message:', util.inspect(err.message, false, null, true))
   }
 }
 
@@ -20,7 +18,7 @@ const startMessaging = async () => {
   const processorAction = message => handleMessage(message, processorReceiver)
   processorReceiver = new MessageReceiver(messageConfig.processorSubscription, processorAction)
   await processorReceiver.subscribe()
-  console.info('Receiver ready to receive processor messages')
+  console.info('Messages processor is ready to receive messages')
 }
 
 module.exports = { startMessaging }
--- app/messaging/send-notification.js
+++ app/messaging/send-notification.js
@@ -4,11 +4,12 @@ const NotifyClient = require('notifications-node-client').NotifyClient
 const sendNotification = async (message) => {
   const notifyClient = new NotifyClient(process.env.NOTIFY_API_KEY)
   await notifyClient.sendEmail(
-    process.env.NOTIFY_TEMPLATE_ID,
+    process.env.NOTIFY_MESSAGES_TEMPLATE_ID,
     process.env.NOTIFY_TEST_EMAIL, {
       personalisation: {
-        heading: message.body.content.heading,
-        content: message.body.content.body
+        heading: message.body.heading,
+        content: message.body.body,
+        messagesHyperlink: `http://localhost:3000/messages/notification/${message.body.sbi}/${message.body.id}/?organisationId=${message.body.organisationId}`
       },
       reference: uuidv4()
     })
