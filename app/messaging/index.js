60efe35e5632549ec245ca09cf363195269c0d5f
AbidemiAdio <98023713+AbidemiAdio@users.noreply.github.com>
2024-08-09 13:51:55 +0100

    Updated code for message processor to carry out specific actions based on message type (#18)

--- app/messaging/index.js
+++ app/messaging/index.js
@@ -1,14 +1,26 @@
 const util = require('util')
 const { messageConfig } = require('../config')
 const { MessageReceiver } = require('ffc-messaging')
-// const { sendNotification } = require('./send-notification')
+const { CRM, SEND_EMAIL } = require('../constants/message-types')
+const { sendNotification } = require('./send-notification')
 const { sendToCrm } = require('./send-to-crm')
 
 const handleMessage = async (message, receiver) => {
   try {
     console.log('Received message: ', message.body)
-    // await sendNotification(message)
-    await sendToCrm(message)
+
+    const type = message.applicationProperties.type
+    switch (type) {
+      case SEND_EMAIL:
+        await sendNotification(message)
+        break
+      case CRM:
+        await sendToCrm(message)
+        break
+      default:
+        throw new Error(`Unknown request type: ${type}`)
+    }
+
     await receiver.completeMessage(message)
   } catch (err) {
     console.error('Error with processing message:', util.inspect(err.message, false, null, true))
